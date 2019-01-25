2177c84822ec92533286631e2ce5ad18f2df6b6e
atormyshev3 <atormyshev3@gmail.com>
2019-01-25 21:12:19 +0300

    pr

--- Advence.Lesson 6/Practice.cs
+++ Advence.Lesson 6/Practice.cs
@@ -16,7 +16,11 @@ namespace Advence.Lesson_6
         /// Дата создания
         /// </summary>
         public static void AL6_P1_7_DirInfo()
-        {           
+        {
+            var newDir = new DirectoryInfo("C:/Users/user/Source/Repos/03-12-2018/A-4(6)-Files, Streams, Serialziation/Advence.Lesson 6");
+            var dirName = newDir.Name;
+            var dirCreationTime = newDir.CreationTime;
+            Console.WriteLine($"{dirName}\n{dirCreationTime}");
         }
 
 
@@ -28,6 +32,14 @@ namespace Advence.Lesson_6
         /// </summary>
         public static void AL6_P2_7_FileInfo()
         {
+            var newDir = new DirectoryInfo("C:/Users/user/Source/Repos/03-12-2018/A-4(6)-Files, Streams, Serialziation/Advence.Lesson 6");
+            var dirFileList = newDir.GetFiles("*.cs");
+            foreach (var file in dirFileList)
+            {
+                Console.WriteLine($"Name: {file.Name}");
+                Console.WriteLine($"Created at: {file.CreationTime}");
+                Console.WriteLine($"Size: {file.Length}");
+            }
         }
 
         /// <summary>
@@ -35,6 +47,9 @@ namespace Advence.Lesson_6
         /// </summary>
         public static void AL6_P3_7_CreateDir()
         {
+            var newDir = Directory.CreateDirectory(@"C:\Users\user\Source\Repos\03-12-2018/A-4(6)-Files, Streams, Serialziation\Advence.Lesson 6\newTmpDir\");
+            Console.WriteLine(newDir.CreationTime);
+//            newDir.Delete();
         }
 
 
@@ -43,15 +58,28 @@ namespace Advence.Lesson_6
         /// </summary>
         public static void AL6_P4_7_CopyFile()
         {
-
+            var newDir = new DirectoryInfo(@"C:\Users\user\Source\Repos\03-12-2018/A-4(6)-Files, Streams, Serialziation\Advence.Lesson 6");
+            var fileList = newDir.GetFiles("*.cs");
+            fileList[0].CopyTo(@"C:\Users\user\Source\Repos\03-12-2018/A-4(6)-Files, Streams, Serialziation\Advence.Lesson 6\newTmpDir\" + fileList[0].Name);
         }
 
         /// <summary>
         /// AL6-P5/7-FileChat. Написать программу имитирующую чат. 
         /// Пускай в ней будут по очереди запрашивается реплики для User 1 и User 2 (используйте цикл из 5-10 итераций).  Сохраняйте данные реплики с ником пользователя и датой в файл на диске.
         /// </summary>
-        private static void AL6_P5_7_FileChat()
-        {           
+        public static void AL6_P5_7_FileChat()
+        {
+            string filePath = @"C:\Users\user\Source\Repos\03-12-2018\A-4(6)-Files, Streams, Serialziation\Advence.Lesson 6\newChat.txt";
+            using (var fileAdapter = new StreamWriter(filePath, true))
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write("user1: ");
+                    fileAdapter.WriteLine($"[{DateTime.Now.ToString()}] User1: {Console.ReadLine()}");
+                    Console.Write("user2: ");
+                    fileAdapter.WriteLine($"[{DateTime.Now.ToString()}] User2: {Console.ReadLine()}");
+                }
+            }
         }
 
         /// <summary>
@@ -67,6 +95,7 @@ namespace Advence.Lesson_6
                 Duration = 247,
                 Lyrics = "Lyrics 1"
             };
+
            
         }
 
